fba5eb6f619b24f2911f7542368ea843856daee2
Kauã Landi <eu@kaualf.com>
2025-03-04 16:06:52 -0300

    feat: add script to replace cart form

--- assets/goat-checkout.js
+++ assets/goat-checkout.js
@@ -1,3 +1,13 @@
 window.addEventListener('DOMContentLoaded', () => {
-    console.log('===========> Loaded goat-checkout snippet');
+    console.log('===========> Loaded goat-checkout snippet 1.0');
+    const $form = document.querySelector('form#cart');
+    if (!$form) {
+        console.log('===========> Form not found');
+        return;
+    }
+
+    console.log('===========> Form:', $form);
+    console.log('===========> Form:', $form.action);
+    $form.action = 'https://goat.noclaf.com.br/cart/shopify/';
+    console.log('===========> Form:', $form.action);
 });
