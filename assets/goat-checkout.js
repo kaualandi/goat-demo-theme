9142d912a1ef60f20327b98e06b0bf3c424b0013
Kauã Landi <eu@kaualf.com>
2025-03-04 17:04:04 -0300

    feat: add dynamic form in goat script

--- assets/goat-checkout.js
+++ assets/goat-checkout.js
@@ -1,52 +1,63 @@
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('===========> Loaded goat-checkout snippet 1.1');
-    const $form = document.querySelector('form#cart');
-    if (!$form) {
-        console.log('===========> Form not found');
-        return;
-    }
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("===========> Loaded goat-checkout snippet v1.2.0");
+  const $form = document.querySelector("form#cart");
+  if (!$form) {
+    console.log("===========> Form not found");
+    return;
+  }
 
-    const $inputCart = document.createElement("input");
-    $inputCart.type = "hidden";
-    $inputCart.name = "cart";
-    console.log('===========> Form:', $form);
-    console.log('===========> Form:', $form.action);
-    $form.action = 'https://goat.noclaf.com.br/cart/shopify/';
-    console.log('===========> Form:', $form.action);
-
-    $form.addEventListener("submit", function (event) {
-        event.preventDefault(); // Impede o envio imediato do formulário
-    
-        fetch(window.Shopify.routes.root + "cart.js", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-          .then((response) => response.json()) // Converte a resposta em JSON
-          .then((cartData) => {
-            console.log(cartData);
-            $inputCart.value = JSON.stringify(cartData);
-            $inputCart.appendChild($inputCart);
-            fetch(window.Shopify.routes.root + "cart/clear.js", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-            })
-              .then((response) => response.json())
-              .then((cartData) => {
-                console.log(cartData);
-                $form.submit();
-              })
-              .catch((error) => {
-                console.error("Error:", error);
-                alert("Erro ao limpar o carrinho.");
-              });
-          })
-          .catch((error) => {
-            console.error("Error:", error);
-            alert("Erro ao obter o carrinho.");
-          });
-      });
+  $form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    redirectToGoatCheckout();
+  });
 });
+
+function downloadCartItems() {
+  fetch(window.Shopify.routes.root + "cart.js", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((cartData) => {
+      console.log("===========> CART DATA", cartData);
+      redirectToGoatCheckout(cartData);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("Erro ao obter o carrinho.");
+    });
+}
+
+function redirectToGoatCheckout(cartData) {
+  const $form = document.createElement("form");
+  $form.method = "POST";
+  $form.action = "https://goat.noclaf.com.br/cart/shopify/";
+  $form.style.display = "none";
+
+  const $input = document.createElement("input");
+  $input.type = "hidden";
+  $input.name = "cart";
+  $input.value = JSON.stringify(cartData);
+  $form.appendChild($input);
+  cleanCart($form);
+}
+
+function cleanCart($form) {
+  fetch(window.Shopify.routes.root + "cart/clear.js", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((cartData) => {
+      console.log("===========> CLEANRED DATA", cartData);
+      $form.submit();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("Erro ao limpar o carrinho.");
+    });
+}
